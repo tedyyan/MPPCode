e4dcd1fda42a1bf4a06e3e841d98d1589f852011
bookcool <sunshuhan666@163.com>
2019-05-28 18:22:50 -0500

    Update Order.java

--- lab2/prob2b/Order.java
+++ lab2/prob2b/Order.java
@@ -4,26 +4,35 @@ class Order{
 	private String orderDate;
 	private List<OrderLine> orderLines;
 
-	public Order(){
+	public Order() {
 		this.orderLines = new LinkedList<OrderLine>();
 	}
-	public void addOrderLine(OrderLine orderLine) {
+
+	public void addOrderLine(Integer linenum, Integer price, Integer quatity) {
+		OrderLine orderLine = new OrderLine(this);
+		orderLine.setLinenum(linenum);
+		orderLine.setPrice(price);
+		orderLine.setQuatity(quatity);
 		orderLines.add(orderLine);
 	}
-	
-	public Long getOrderNum(){
+
+	public Long getOrderNum() {
 		return orderNum;
 	}
-	public void setOrderNum(Long orderNum){
+
+	public void setOrderNum(Long orderNum) {
 		this.orderNum = orderNum;
 	}
-	public String getOrderDate(){
+
+	public String getOrderDate() {
 		return orderDate;
 	}
-	public void setOrderDate(String orderDate){
+
+	public void setOrderDate(String orderDate) {
 		this.orderDate = orderDate;
 	}
-	public List<OrderLine> getOrderLines(){
+
+	public List<OrderLine> getOrderLines() {
 		return orderLines;
 	}
 }
