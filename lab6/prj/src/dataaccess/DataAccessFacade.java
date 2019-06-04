f74b59e4e8081f879e3b0af4b46be595e763c100
bookcool <sunshuhan666@163.com>
2019-06-04 10:42:11 -0500

    add system OS adapt

--- lab6/prj/src/dataaccess/DataAccessFacade.java
+++ lab6/prj/src/dataaccess/DataAccessFacade.java
@@ -13,6 +13,7 @@ import business.Book;
 import business.BookCopy;
 import business.LibraryMember;
 import dataaccess.DataAccessFacade.StorageType;
+import utility.OSinfo;
 
 
 public class DataAccessFacade implements DataAccess {
@@ -22,7 +23,7 @@ public class DataAccessFacade implements DataAccess {
 	}
 	
 	public static final String OUTPUT_DIR = System.getProperty("user.dir") 
-			+ "\\src\\dataaccess\\storage";
+			+ (OSinfo.isMacOSX() ? "/src/dataaccess/storage" : "\\src\\dataaccess\\storage");
 	public static final String DATE_PATTERN = "MM/dd/yyyy";
 	
 	//implement: other save operations
