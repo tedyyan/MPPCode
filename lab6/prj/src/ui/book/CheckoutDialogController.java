d29456910215718a656d51ace31f8ba533bedc37
bookcool <sunshuhan666@163.com>
2019-06-05 14:17:29 -0500

    update checkout

--- lab6/prj/src/ui/book/CheckoutDialogController.java
+++ lab6/prj/src/ui/book/CheckoutDialogController.java
@@ -20,6 +20,7 @@ import business.person.MemberBizServiceInterface;
 import dataaccess.DataAccess;
 import dataaccess.DataAccessFacade;
 import dataaccess.User;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -49,10 +50,18 @@ public class CheckoutDialogController {
 	
 	@FXML
 	private TableView<CheckRecordEntry> checkoutRecordTableView;
-	
+
 	@FXML
-	TableColumn<CheckRecordEntry, String> column1 = new TableColumn<>();
+	private TableColumn<CheckRecordEntry, String> memberIDColumn;
+	@FXML
+	private TableColumn<CheckRecordEntry, String> isbnColumn;
+	@FXML
+	private TableColumn<CheckRecordEntry, String> dueDateColumn;
+	@FXML
+	private TableColumn<CheckRecordEntry, String> checkoutDateColumn;
     
+	private ObservableList<CheckRecordEntry> observableList = FXCollections.observableArrayList();
+	
 	private Stage dialogStage;
 
 	private BookBizServiceInterface bookBizService = BookBizService.getBookBizServiceInstance();
@@ -64,10 +73,10 @@ public class CheckoutDialogController {
 	 */
 	@FXML
 	private void initialize() {
-//		memberIDColumn.setCellValueFactory(cellData -> new SimpleStringProperty(memberIdField.getText()));
-//		isbnColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getBookCopy().getBook().getIsbn()));
-//		dueDateColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getDueDate().toString()));
-//		checkoutDateColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getCheckOutDate().toString()));
+		memberIDColumn.setCellValueFactory(cellData -> new SimpleStringProperty(memberIdField.getText()));
+		isbnColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getBookCopy().getBook().getIsbn()));
+		dueDateColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getDueDate().toString()));
+		checkoutDateColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getCheckOutDate().toString()));
         
 	}
 
@@ -88,15 +97,21 @@ public class CheckoutDialogController {
 	 */
 	@FXML
 	private void handleAdd() {
-		
+		LibraryMember member = memberBizService.FindPersonByMemberID(memberIdField.getText());
+		if(member == null) {
+			resultField.setText("member not exit!");
+			return ;
+		}
 		Book book = bookBizService.getBookByISBN(isbnField.getText());
-		LibraryMember member = memberBizService.FindPersonByMemberID(memberIdField.getText()); 
-		if(book == null || member == null) {
-			resultField.setText("book or member not exit!");
+		if(book == null) {
+			resultField.setText("book not exit!");
 			return ;
 		}
 		BookCopy bookCopy = book.getNextAvailableCopy();
-		
+		if(bookCopy == null) {
+			resultField.setText("bookCopy not available!");
+			return ;
+		}
 		CheckRecordEntry recordEntry = new CheckRecordEntry(new Date(), bookCopy);
 		Calendar calendar = Calendar.getInstance();
 		calendar.setTime(new Date());
@@ -107,7 +122,8 @@ public class CheckoutDialogController {
 		
 		bookCopy.setCheckoutRecordEntry(recordEntry);
 		bookCopy.changeAvailability();
-		ObservableList<CheckRecordEntry> observableList = FXCollections.observableArrayList(recordEntry);
+		
+		observableList.add(recordEntry);
 		checkoutRecordTableView.setItems(observableList);
 
 		resultField.setText("book checkout success!");
