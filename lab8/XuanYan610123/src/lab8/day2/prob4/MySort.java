70d2310d1ebf6acf07a9775b51480635709ffe18
Teddy Yan <tedyyan@hotmail.com>
2019-06-12 15:17:55 -0500

    day22

--- lab8/XuanYan610123/src/lab8/day2/prob4/MySort.java
+++ lab8/XuanYan610123/src/lab8/day2/prob4/MySort.java
@@ -1,13 +1,13 @@
 package lab8.day2.prob4;
 
-import java.util.List;
+import java.util.Arrays;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class MySort {
 	public static void main(String[] args) {
 		String[] names = {"Alexis", "Tim", "Kyleen", "KRISTY"};
-		List<String> kStream = Stream.of(names).sorted(String::compareToIgnoreCase).collect(Collectors.toList());
-		System.out.println(kStream);
+		Arrays.sort(names,String::compareToIgnoreCase);
+		System.out.print(Stream.of(names).collect(Collectors.joining(" ")));
 	}
 }
