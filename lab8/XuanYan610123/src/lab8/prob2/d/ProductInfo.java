9ac7c1f8bacc7a2f64ba40b107d932fbe72a5b7b
Teddy Yan <tedyyan@hotmail.com>
2019-06-12 10:10:18 -0500

    move

--- lab8/XuanYan610123/src/lab8/prob2/d/ProductInfo.java
+++ lab8/XuanYan610123/src/lab8/prob2/d/ProductInfo.java
@@ -6,16 +6,13 @@ import java.util.List;
 
 import lab8.prob2.Product;
 public class ProductInfo {
-	static enum SortMethod {
-		BYTITLE, BYPRICE
-	};
 	
-	public void sort(List<Product> emps, final SortMethod method) {
+	public void sort(List<Product> emps) {
 		Collections.sort(emps, (o1,o2)->{
-			if(method == SortMethod.BYTITLE) {
-				return o1.getTitle().compareTo(o2.getTitle());
+			if(o1.getTitle().equals(o2.getTitle())) {
+				return Integer.valueOf(o1.getModel()).compareTo(Integer.valueOf(o2.getModel()));
 			} else {
-				return Double.valueOf(o1.getPrice()).compareTo(Double.valueOf(o2.getPrice()));
+				return o1.getTitle().compareTo(o2.getTitle());
 			}
 		});
 	}
@@ -23,16 +20,13 @@ public class ProductInfo {
 		List<Product> products = new ArrayList<Product>();
 		products.add(new Product("Carpet", 23.0, 2));
 		products.add(new Product("Air Condition", 523.0, 1));
-		products.add(new Product("Fridge", 423.0, 21));
-		products.add(new Product("Funiture", 223.0, 12));
+		products.add(new Product("Air Condition", 423.0, 21));
+		products.add(new Product("Air Condition", 223.0, 12));
 		products.add(new Product("Chair", 123.0, 32));
 		ProductInfo info = new ProductInfo();
-		info.sort(products, SortMethod.BYPRICE);
+		info.sort(products);
 		System.out.println(products);
 
-		System.out.println("----------------------------");
-		info.sort(products, SortMethod.BYTITLE);
-		System.out.println(products);
 	}
 	
 }
